3092b87791358e3d87946fa3177f76ae68ee5713
Froraut <iliaborovoy2002@gmail.com>
2021-06-04 23:50:12 +0300

    ALL LABS

---
+++ LAB6CS/LAB6CS/IElectricMotor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB6CS
+{
+    interface IElectricMotor
+    {
+        double BatteryCapacity { get; }
+        double CurrentEnergy { get; }
+        double EnergyConsumption { get; }
+
+        void ChargeBatteryUp();
+        void ChargeBatteryUp(double amount);
+        bool RunOnElectricMotor(double meters);
+    }
+}
